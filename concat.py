ca80ae4bc19b7c191f70454146a6fe30a908f2d1
Khalil2506 <113655824+Khalil2506@users.noreply.github.com>
2024-07-29 19:48:50 -0500

    update 29/07/2024

--- concat.py
+++ concat.py
@@ -31,13 +31,13 @@ def add_id_player_df(id_competition, id_season, df_player):
         coincidencia_nickname = process.extractOne(
             nombre_excel, lineups_df['player_nickname'], scorer=fuzz.token_set_ratio
         )
-        if coincidencia_nickname and coincidencia_nickname[1] > 75:
+        if coincidencia_nickname and coincidencia_nickname[1] > 80:
             return lineups_df.loc[lineups_df['player_nickname'] == coincidencia_nickname[0], 'player_id'].values[0]
 
         coincidencia_name = process.extractOne(
             nombre_excel, lineups_df['player_name'], scorer=fuzz.token_set_ratio
         )
-        if coincidencia_name and coincidencia_name[1] > 75:
+        if coincidencia_name and coincidencia_name[1] > 80:
             return lineups_df.loc[lineups_df['player_name'] == coincidencia_name[0], 'player_id'].values[0]
         
         return None
